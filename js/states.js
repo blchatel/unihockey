5c64cb921b0d86f29146310310702fa468128ef4
blchatel <bastien.l.chatelain@gmail.com>
2018-10-11 09:59:47 +0200

    edit default level with a single frame

--- js/states.js
+++ js/states.js
@@ -11,27 +11,5 @@ const defaultLevel = {
             {id:6, x: 9, y: 9, r: 0.5, team: "#28a745", number: "8"},
             {id:7, x: 2, y: 11, r: 0.5, team: "#28a745", number: "9"},
         ],
-/*
-        [// frame 1
-            {id:0, x: 9, y: 22, r: 0.5, team: "red", number: "1"},
-            {id:1, x: 9, y: 6, r: 0.5, team: "blue", number: "1"},
-            {id:2, x: 10, y: 16, r: 0.5, team: "red", number: "4"},
-            {id:3, x: 8, y: 18, r: 0.5, team: "red", number: "5"},
-            {id:4, x: 8, y: 20, r: 0.5, team: "red", number: "6"},
-            {id:5, x: 8, y: 8, r: 0.5, team: "blue", number: "7"},
-            {id:6, x: 10, y: 10, r: 0.5, team: "blue", number: "8"},
-            {id:7, x: 3, y: 12, r: 0.5, team: "blue", number: "9"},
-        ],
-        [// frame 2
-            {id:0, x: 10, y: 23, r: 0.5, team: "red", number: "1"},
-            {id:1, x: 10, y: 7, r: 0.5, team: "blue", number: "1"},
-            {id:2, x: 11, y: 17, r: 0.5, team: "red", number: "4"},
-            {id:3, x: 9, y: 19, r: 0.5, team: "red", number: "5"},
-            {id:4, x: 9, y: 21, r: 0.5, team: "red", number: "6"},
-            {id:5, x: 9, y: 9, r: 0.5, team: "blue", number: "7"},
-            {id:6, x: 11, y: 11, r: 0.5, team: "blue", number: "8"},
-            {id:7, x: 4, y: 13, r: 0.5, team: "blue", number: "9"},
-        ],
-*/
-    ],
+    ]
 }
